90b02c5f19e3e27e2258d12fe3f78f4dcec2340e
EliMeitner <eli.meitner@toluna.com>
2024-07-07 14:49:27 +0300

    move to v2 modules

--- main.tf
+++ main.tf
@@ -1,16 +1,26 @@
 locals {
+  config                 = var.config.app.codeless_pipeline
   artifacts_bucket_name = "s3-codepipeline-${var.app_name}-${var.env_type}"
+  app_name               = var.app_name == null ? local.config.app_name : var.app_name
+  from_env               = var.from_env == null ? local.config.from_env : var.from_env
+  env_type               = var.env_type == null ? local.config.env_type : var.env_type
+  pipeline_type          = var.pipeline_type == null ? local.config.pipeline_type : var.pipeline_type
+  environment            = var.environment == null ? local.config.environment : var.environment
+  env_name               = split("-", local.environment)[0]
+  run_integration_tests  = var.run_integration_tests == null ? local.config.run_integration_tests : var.run_integration_tests
+  source_repository      = var.source_repository == null ? "tolunaengineering/${local.app_name}" : var.source_repository
+  vpc_config             = var.vpc_config.vpc_id == "NULL" ? merge(local.config.vpc_config, { security_group_ids = var.security_group_ids }) : var.vpc_config
 }
 
 module "ci-cd-code-pipeline" {
   source                       = "./modules/ci-cd-codepipeline"
-  env_name                     = var.env_name
-  app_name                     = var.app_name
-  pipeline_type                = var.pipeline_type
-  source_repository            = var.source_repository
+  env_name                     = local.env_name
+  app_name                     = local.app_name
+  pipeline_type                = local.pipeline_type
+  source_repository            = local.source_repository
   s3_bucket                    = local.artifacts_bucket_name
   test_codebuild_projects      = [module.test.attributes.name]
-  merge_codebuild_projects     = var.pipeline_type != "dev" ? ["Merge-Waiter"] : []
+  merge_codebuild_projects     = local.pipeline_type != "dev" ? ["Merge-Waiter"] : []
   depends_on = [
     module.test
   ]
@@ -18,20 +28,20 @@ module "ci-cd-code-pipeline" {
 
 module "test" {
   source                                = "./modules/test"
-  env_name                              = var.env_name
-  env_type                              = var.env_type
+  env_name                              = local.environment
+  env_type                              = local.env_type
   codebuild_name                        = "test-${var.app_name}"
-  source_repository                     = var.source_repository
-  s3_bucket                             = "s3-codepipeline-${var.app_name}-${var.env_type}"
+  source_repository                     = local.source_repository
+  s3_bucket                             = "s3-codepipeline-${local.app_name}-${local.env_type}"
   privileged_mode                       = true
   environment_variables                 = var.environment_variables
   buildspec_file                        = templatefile("test_buildspec.yml.tpl",
-  { ENV_NAME = split("-",var.env_name)[0],
-    ENVIRONMENT = var.env_name,
-    FROM_ENV = var.from_env,
-    APP_NAME = var.app_name,
-    ENV_TYPE = var.env_type,
-    PIPELINE_TYPE = var.pipeline_type
-    REPO_NAME = var.source_repository
+  { ENV_NAME = local.env_name,
+    ENVIRONMENT = local.environment,
+    FROM_ENV = local.from_env,
+    APP_NAME = local.app_name,
+    ENV_TYPE = local.env_type,
+    PIPELINE_TYPE = local.pipeline_type
+    REPO_NAME = local.source_repository
     })
 }
--- variables.tf
+++ variables.tf
@@ -1,36 +1,50 @@
+variable "config" {
+}
+
 variable "env_name" {
     type = string
+    default  = null
+}
+
+variable "environment" {
+    type = string
+    default  = null
 }
 
 variable "from_env" {
     type = string
+    default  = null
 }
 
 variable "app_name" {
     type = string
+    default  = null
 }
 
 variable "env_type" {
     type = string
+    default  = null
 }
 
 variable "run_integration_tests" {
     type = bool
-    default = false
+    default  = null
 }
 
 variable "source_repository" {
-    type = string
+  type     = string
+  default  = null
 }
 
 variable "environment_variables" {
- type = map(string)
- default = {
- }
+  type = map(string)
+  default = {
+  }
 }
 
 variable "pipeline_type" {
   type = string
+  default  = null
 }
 
 variable "termination_wait_time_in_minutes" {
@@ -38,5 +52,14 @@ variable "termination_wait_time_in_minutes" {
 }
 
 variable "vpc_config" {
-  default = {}
+  default = {
+     vpc_id             = "NULL",
+      subnets            = [],
+      security_group_ids = []
+  }
+}
+
+variable "security_group_ids" {
+  type = list(string)
+  default = []
 }
